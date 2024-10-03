46aab88e988151b157a64b512b7725781021d517
Marina <climovicimarina@gmail.com>
2024-10-03 10:07:14 +0200

    weather output template

--- weather_forecast/app/urls.py
+++ weather_forecast/app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='cities'),
-    path('weather/', views.weather, name='weather'),
+    path('weather', views.weather, name='weather'),
 ]
