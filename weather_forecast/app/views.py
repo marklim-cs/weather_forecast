5cfc89daf75e35a7f47e8fc7e03bc89896cce68e
Marina <climovicimarina@gmail.com>
2024-10-06 18:45:42 +0200

    no input exception handled

--- weather_forecast/app/views.py
+++ weather_forecast/app/views.py
@@ -16,26 +16,27 @@ def weather(request):
     current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric"
     forecast_url = "https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}&units=metric"
 
-    if request.method == "POST":
-        city1 = request.POST['city1']
-        city2 = request.POST.get('city2', None) #get() allows to provide a default value, works for optional fields
+    try:
+        if request.method == "POST":
+            city1 = request.POST['city1']
+            city2 = request.POST.get('city2', None) #get() allows to provide a default value, works for optional fields
 
-        weather_current1, daily_forecast1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_url)
-        if city2:
-            weather_current2, daily_forecast2 = fetch_weather_and_forecast(city2, API_KEY, current_weather_url, forecast_url)
-        else:
-            weather_current2, daily_forecast2 = None, None
+            weather_current1, daily_forecast1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_url)
+            if city2:
+                weather_current2, daily_forecast2 = fetch_weather_and_forecast(city2, API_KEY, current_weather_url, forecast_url)
+            else:
+                weather_current2, daily_forecast2 = None, None
 
-        context = {
-            "weather_current1": weather_current1, 
-            "daily_forecast1": daily_forecast1, 
-            "weather_current2": weather_current2, 
-            "daily_forecast2": daily_forecast2, 
-        }
+            context = {
+                "weather_current1": weather_current1, 
+                "daily_forecast1": daily_forecast1, 
+                "weather_current2": weather_current2, 
+                "daily_forecast2": daily_forecast2, 
+            }
 
-        return render(request, "weather.html", context)
-    else:
-        return render(request, "weather.html")
+            return render(request, "weather.html", context)
+    except:
+        return render(request, "index.html")
 
 def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
     response = requests.get(current_weather_url.format(city, api_key)).json()
@@ -66,10 +67,7 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url)
         descriptions = (data['weather'][0]['description'] for data in data_list)
         most_frequent_description = count_element_frequency(descriptions)
 
-        #print(str(descriptions))
-
         icons = (data['weather'][0]['icon'] for data in data_list)
-        #print(icons)
         most_frequent_icon = count_element_frequency(icons)
 
         daily_forecast.append({
